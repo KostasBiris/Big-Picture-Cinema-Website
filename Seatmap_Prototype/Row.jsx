61edf432b906c0777be015f961c38018e1fb73f4
Raulian-Ionut Chiorescu <raul_mac@Raulian-Ionuts-MacBook-Pro.local>
2021-02-14 20:17:40 +0200

    Added Skeleton Files for the Seatmap Prototype created in ReactJS

---
+++ Seatmap_Prototype/Row.jsx
@@ -0,0 +1,9 @@
+import React, { PropTypes as T } from 'react';
+import Seat from './Seat';
+import Blank from './Blank';
+import RowNumber from './RowNumber';
+import cx from 'classnames';
+
+export default class Row extends React.Component{
+
+}
