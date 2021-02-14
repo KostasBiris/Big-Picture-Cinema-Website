960c34a20a53f7f8d6773642e4e4847157d30eb4
Marina Konstantina Polychronidou <sc19mkp@leeds.ac.uk>
2021-02-14 22:32:39 +0000

    Tab buttons, search bar, login, signup, header

--- main.py
+++ main.py
@@ -2,5 +2,11 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask import redirect
-from pythonlogin import CreateDatabase
 app = Flask(__name__)
+
+@app.route('/')
+def mainpage():
+    return render_template('customers_main_interface.html')
+
+if __name__ == '__main__':
+    app.run(debug=False, host='localhost', port=4000)
