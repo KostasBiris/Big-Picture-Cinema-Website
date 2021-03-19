cfda30c623e9a3c1dc79659f4ba945107d95fac7
Raulian-Ionut Chiorescu <raul_mac@Raulian-Ionuts-MacBook-Pro.local>
2021-03-19 13:15:26 +0200

    Fixed css issue

--- react-flask-app/src/Seatmap/Screen3.js
+++ react-flask-app/src/Seatmap/Screen3.js
@@ -3,11 +3,7 @@ import React from 'react';
 import Seatmap from './Seatmap.jsx';
 import main from '../static/main.css';
 import './Style/Seatmap.scss';
-import logo from '../static/finlogo.png';
-import headerbanner from '../static/headerbanner.png';
-import follow from '../static/follow.png';
-import search from '../static/search.png';
-import usericon from '../static/usericon.png';
+import Banner from '../components/Banner.js';
 
 
 const rows = [
@@ -34,61 +30,14 @@ export default function Screen3() {
       <head>
       <link rel="stylesheet" type="text/css" href={main} />
       </head>
+      <Banner/>
 
-
-    <img src={logo} style={{top:'1px',width:'300px',height:'190px'}}></img>
-    <img src={headerbanner} style={{position:'absolute',top:'10px',left:'340px',width:'980px',height:'105px'}}></img>
-    <img src={follow} style={{position: 'absolute', top: '10px', left: '1380px', width:'100px',height:'105px'}}></img>
-    <div class="text">
-  
-        <button class="tab_background" style={{position:'absolute',top:'125px',left:'340px',width:'150px',height:'40px'}}>WHAT'S NEW </button>
-        <button class="tab_background" style={{position:'absolute',top:'125px',left:'510px',width:'150px',height:'40px'}}>TICKETS</button>
-        <button class="tab_background" style={{position:'absolute',top:'125px',left:'680px',width:'150px',height:'40px'}}>SCREENS</button>
-        <button class="tab_background" style={{position:'absolute',top:'125px',left:'850px',width:'150px',height:'40px'}}>INFO</button>
-    </div>
-    <form action="{{ url_for('mainpage') }}" method="post">
-     
-        <input class="search_bar" name="query" id="query" type="text" placeholder="Search here.." style={{position:'absolute',top:'125px',left:'1020px'}}/>
     
-        <input class="search_icon" type="image" src={search} style={{position:'absolute',top:'125px',left:'1270px',width:'50px',height:'40px'}}/>
-    </form>
-    <div>
-        <input id="account" type="image" src={usericon} style={{position:'absolute',top:'125px',left:'1340px',width:'40px',height:'40px'}} />
-        <script type="text/javascript">
-        document.getElementById("account").onclick = function () {
-            //location.href = "{{ url_for('account') }}"
-        };
-        </script>
-        <button type="submit" id="login" class="text_button"
-        style={{position:'absolute',top:'125px',left:'1390px',width:'60px',height:'40px'}}>LOG IN</button>
-        <script type="text/javascript">
-        document.getElementById("login").onclick = function () {
-            //location.href = "{{ url_for('login') }}"
-        };
-        </script>
-        <button id="register" type="submit" class="text_button"style={{position:'absolute',top:'125px',left:'1460px',width:'60px',height:'40px'}}>SIGN UP</button>
-        <script type="text/javascript">
-        document.getElementById("register").onclick = function () {
-            //location.href = "{{ url_for('register') }}"
-        };
-        </script>
-        <input type="image" src={usericon}
-            style={{position:'absolute',top:'125px',left:'1340px',width:'40px',height:'40px'}} />
-       
-        <button class="text_button" style={{position:'absolute',top:'125px',left:'1390px',width:'60px',height:'40px'}}>LOG
-            IN</button>
-        <button class="text_button" style={{position:'absolute',top:'125px',left:'1460px',width:'60px',height:'40px'}}>SIGN
-            UP</button>
-            <button class="text_button" style={{position:'absolute',top:'125px',left:'1530px',width:'60px',height:'40px'}}>LOG OUT</button>
-      
-   
-    </div> 
-
-      <div className="Seatmap" style={{marginLeft:'400px',marginTop:'50px'}}>
+      <div className="Seatmap" style={{marginLeft:'350px',marginTop:'50px'}}>
       <Seatmap rows={rows} maxReservableSeats={10} alpha />   
       </div>
       <div class="header_text">
-        <h1 style={{marginLeft:'700px', left:'3rem', color: '#4e5b60'}}>SCREEN</h1>
+        <h1 style={{marginLeft:'650px', left:'3rem', color: '#4e5b60'}}>SCREEN</h1>
     </div>   
       </body>
     </React.Fragment>
