8341b150cb5c5f307fb7a5840cfe567bb5cfeed4
Juan Camacho Mohedano <sc19jcm@leeds.ac.uk>
2021-03-20 18:20:29 +0100

    changed the fetching methods in MoviePage to fetch our db. Passing the state to BookTicketsPage

--- react-flask-app/src/Pages/MoviePage.js
+++ react-flask-app/src/Pages/MoviePage.js
@@ -10,10 +10,14 @@ class MoviePage extends React.Component {
         super(props);
         this.title = props.match.params.title;
         this.movieID = props.match.params.movieID;
-        this.state = {IP: null, auth: false, returnedData: [], directors: [], actors: [], writers: [], genres: [], movieURL: ''};
+        this.state = {IP: null, auth: false, 
+                      returnedData: [], 
+                      movieURL: '',
+                      fromMoviePage: true};
         //Bind our method.
         this.getMovieData = this.getMovieData.bind(this);
         this.getClientIP = this.getClientIP.bind(this);
+        this.goBookTickets = this.goBookTickets.bind(this);
         //Call our method.
         this.getMovieData();
         this.getClientIP();
@@ -24,53 +28,21 @@ class MoviePage extends React.Component {
         })();
 
     }
+    
+    componentDidMount = () => {
+        window.addEventListener('load', this.getMovieData);
+    }
+
+    componentDidMount = () => {
+        window.removeEventListener('load', this.getMovieData);
+    }
 
     //Method for getting the data of the specific requested movie.
-    getMovieData = (data) => {
-        this.setState({ returnedData : data})
-        let directors = []
-        let actors = []
-        let writers = []
-        let genres = []
-        let video = []
-        if(this.state.returnedData.credits){
-            this.state.returnedData.credits.crew.forEach(function(entry){
-                    if (entry.job === 'Director') {
-                        directors.push(entry.name);
-                    }
-                    else if (entry.job === 'Writer') {
-                        writers.push(entry.name);
-                    }
-                })
-            this.state.returnedData.credits.cast.forEach(function(entry){
-                if (entry.known_for_department == 'Acting'){
-                    actors.push(entry.original_name)
-                }
-            })
-            this.setState({directors: directors});
-            this.setState({actors: actors});
-            this.setState({writers: writers});
-        }
-        else
-            console.log("not working");
-        if(this.state.returnedData.credits){
-            this.state.returnedData.genres.forEach(function(entry){
-                    genres.push(entry.name);
-                })
-            this.setState({genres: genres});
-        }
-        if(this.state.returnedData.videos)
-            this.state.returnedData.videos.results.forEach(function(entry){
-            video.push(entry.key);
-            })
-        // this.setState({videoKeys : video[0]});
-        this.setState({movieURL: "https://www.youtube.com/embed/" +video[0] + "/?controls=1"})
-
-    }
+    getMovieData = () => {
         // to fetch from our database
-        //Invoke a request to our rest API to get the data of the movie.
-        /*var fet = '/movie/' + this.title + '/page';
-        fetch(fet, {
+        // Invoke a request to our rest API to get the data of the movie.
+        var route = '/movie/' + this.title + '/page';
+        fetch(route, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -78,38 +50,47 @@ class MoviePage extends React.Component {
             body: JSON.stringify({ movie: this.title })
         })
             .then(response => response.json()).then(data => {
-                this.setState({ data: data })
+
+                this.setState({ returnedData: data });
+                console.log(this.state.returnedData);
+                
             });
-    }*/
+
+    }
+        
+
+    goBookTickets = (e) => {
+        let go = '/book';
+        this.props.history.push(go, this.state);
+    }
+
+
     render() {
         return (
             <React.Fragment>
                 <head>
                     <link rel="stylesheet" type="text/css" href={main} />
                 </head>
-
-                {/* call SearchIMDB to get movie data     */}
-                <SearchIMDB onGetMovie={this.getMovieData} movieName={this.title} getAllMovies={'False'} movieID={this.movieID}/>
                 <div className="header_text"></div>
                     <h1 style = {{position:'absolute', left:'5rem', color: '#4e5b60' }}>{this.state.returnedData.original_title}</h1>
                     <br /><br /><br /><br /><br /><br /><br /><br />
                     <h2 className="text" style={{position:'absolute', left:'35rem', color:'#4e5b60'}}>Movie Description</h2>
                     <br /><br />
-                    <h2 className="text_description" style={{position:'absolute'}}> {this.state.returnedData.overview} </h2>
+                    <h2 className="text_description" style={{position:'absolute'}}> {this.state.returnedData.blurb} </h2>
                     <img className="movie_page" src={'https://image.tmdb.org/t/p/w500/' + this.state.returnedData.poster_path} style={{left: '5rem'}}/>
                     <div className="text"></div>
                     <table className="position_table">
                             <tr>
                             <th>Director/s: </th>
-                            <td data-th="Director/s">{this.state.directors.join(', ')}</td>
+                            <td data-th="Director/s">{this.state.returnedData.director}</td>
                             </tr>
                          <tr>
                          <th>Writer/s: </th>
-                        <td data-th="Writer/s">{this.state.writers.join(', ')}</td>
+                        <td data-th="Writer/s">{this.state.returnedData.writers}</td>
                         </tr>
                         <tr>
                         <th>Actors: </th>
-                        <td data-th="Actors">{this.state.actors.join(', ')}</td>
+                        <td data-th="Actors">{this.state.returnedData.leadactors}</td>
                         </tr>
                         <tr>
                         <th>Runtime: </th>
@@ -120,21 +101,21 @@ class MoviePage extends React.Component {
                             <td data-th="Release date">{this.state.returnedData.release_date}</td>
                         </tr>
                         <th>Genre: </th>
-                        <td data-th="Genre">{this.state.genres.join(', ')}</td>
+                        <td data-th="Genre">{this.state.returnedData.genres}</td>
                         <tr>
                         <th>Rating: </th>
-                        <td data-th="Certificate">{this.state.returnedData.vote_average}</td>
+                        <td data-th="Certificate">{this.state.returnedData.certificate}</td>
                         </tr>
                     </table>
                     <br /><br />
                     <div>
                         <br />
                     <div className="video_frame">
-                            <iframe width="600px" height="335px" src = {this.state.movieURL}>
+                            <iframe width="600px" height="335px" src = {"https://www.youtube.com/embed/" + this.state.returnedData.youtube_key + "/?controls=1"}>
                             </iframe>
                     </div>
                     <div class="text">
-                        <button className="tab_background" style={{position:'absolute',top:'80%',left:'1300px',width:'180px',height:'60px'}}>BOOK TICKETS</button>
+                        <button onClick={this.goBookTickets} className="tab_background" style={{position:'absolute',top:'80%',left:'1300px',width:'180px',height:'60px'}}>BOOK TICKETS</button>
                     </div>
                 </div>
             </React.Fragment>
